8720d7226ed78c5af4e191be0cccd9141e528f49
EugenD <dmitrichenko.ev@gmail.com>
2018-08-16 00:27:12 +0300

    что-то пошло не так с параметром --amend

--- src/ru/dev/String/SString.java
+++ src/ru/dev/String/SString.java
@@ -1,4 +1,13 @@
 package ru.dev.String;
 
 public class SString {
+
+
+    public static void main(String[] args) {
+        String str = "    Some String     ";
+        System.out.println(str.charAt(1));
+        System.out.println(str.trim());
+    }
+
+
 }
